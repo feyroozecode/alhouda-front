73e9c52fe148fd547fa6e142bb0ab905c218ee64
ibrahmad <ibrahmad18@gmail.com>
2023-09-02 07:33:47 +0100

    configure flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,15 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue, js, ts, jsx, tsx }",
+    'node_modules/flowbite-vue/**/*.{js, jsx, ts, tsx }',
+    'node_modules/flowbite/**/*.{js, jsx, ts, tsx }'
 
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
