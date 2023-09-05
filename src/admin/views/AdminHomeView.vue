e1bfc8da6b7bdeeb484377415e7a4bfff225a848
ibrahmad <ibrahmad18@gmail.com>
2023-09-05 07:01:21 +0100

    adding a link on admin to return client part and initiate a client left and right sidebra

--- src/admin/views/AdminHomeView.vue
+++ src/admin/views/AdminHomeView.vue
@@ -2,9 +2,20 @@
     <div class="w-full h-full">
        
         <div class="flex justify-center ">
-            <h1 class="text-green-700"> {{ title }} </h1> <br/>
+            <br/>
+            <h1 class="text-green-700"> {{ title }} </h1> 
+            <br/>
             Alhouda is a Platforme for islamic courses .
+
+           
+            <div class="mt-25">
+                <router-link to="/">return <b class="text-green-500">Home Page</b> </router-link>
+            </div>
+
         </div>
+
+
+
     </div>
 </template>
 
