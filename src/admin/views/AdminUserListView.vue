9e075b58b8f0383391dc724de66b686bc1f99395
ibrahmad <ibrahmad18@gmail.com>
2023-09-14 18:13:29 +0100

    first version with fetch user from server

--- src/admin/views/AdminUserListView.vue
+++ src/admin/views/AdminUserListView.vue
@@ -1,8 +1,8 @@
 
 <script setup lang="ts">
 import { ref, onMounted }  from  'vue'
-import { Table } from 'flowbite-vue'
-import axios     from 'axios'
+import { Table, TableHead, TableBody, TableRow, TableHeadCell, TableCell} from 'flowbite-vue'
+import    axios  from 'axios'
 import { GET_ALL_USERS } from '@/data/static';
 
 onMounted(() => {
@@ -13,9 +13,10 @@ onMounted(() => {
 const users = ref([])
 
 const fetchAllUsers: any = () => {
-    axios.get(GET_ALL_USERS)
-        .then((response) => {
-            users.value = response.data
+    axios.get( GET_ALL_USERS )
+        .then((response: any) => {
+            users.value = response.data.users
+            console.table(  users.value )
         })
         .catch((error: any) => {
             console.error('Error fetching user data', error)
@@ -25,18 +26,18 @@ const fetchAllUsers: any = () => {
 </script>
 
 <template>
-    <div  class="flex items-center">
-        <Table>
-            <table-head>
-                <table-cell>ID</table-cell>
-                <table-cell>UserName</table-cell>
-                <table-cell>Email</table-cell>
-                <table-cell>Roles</table-cell>
+    <div  class="flex items-center m-10">
+        <Table striped>
+            <table-head class="">
+                <table-head-cell>ID</table-head-cell>
+                <table-head-cell>UserName</table-head-cell>
+                <table-head-cell>Email</table-head-cell>
+                <table-head-cell>Roles</table-head-cell>
             </table-head>
             <table-body>
                 <table-row v-for="user in users " :key="user._id">
                     <table-cell> {{ user._id }} </table-cell>
-                    <table-cell> {{ user.name }} </table-cell>
+                    <table-cell> {{ user.username }} </table-cell>
                     <table-cell> {{ user.email }} </table-cell>
                     <table-cell> {{ user.role }} </table-cell>
                 </table-row>
